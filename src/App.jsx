1a8457d1cd594845cc0ca80c52bb2c4092a7a0d7
Mohammedshibili10 <shibiliek516@gmail.com>
2026-01-10 23:03:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import './App.css'
 import ElvHome from './pages/ElvHome'
 import Elvitsolution from './pages/Elvitsolution'
 import CinemaAV from './pages/CinemaAV'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
  
 
   return (
     <>
-      {/* <ElvHome/> */}
-      {/* <Elvitsolution/> */}
-      <CinemaAV/>
+    <Routes>
+       <Route path='/' element={<ElvHome/>}/>
+       <Route path='/itsolution' element={<Elvitsolution/>}/>
+       <Route path='/cinema' element={<CinemaAV/>}/>
+
+
+    </Routes>
+     
     </>
   )
 }
