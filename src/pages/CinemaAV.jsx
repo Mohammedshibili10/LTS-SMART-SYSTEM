2d8fedee8339beb75e1be9ac51e42e8999e83168
Mohammedshibili10 <shibiliek516@gmail.com>
2026-01-11 23:29:19 +0530

    video updated

--- src/pages/CinemaAV.jsx
+++ src/pages/CinemaAV.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import itbackground from '../assets/images/elvitbackground.jpg'
+import cinema from '../assets/videos/cinema.mp4'
 import Footer from '../components/Footer'
 import CinemaAVServices from '../components/CinemaAVServices'
 import { Link } from 'react-router-dom'
@@ -7,8 +7,9 @@ import { Link } from 'react-router-dom'
 export default function CinemaAV() {
   return (
    <div>
-           <div style={{ backgroundImage: `url(${itbackground})` }} className="relative bg-cover bg-center min-h-screen "  >
-              <div className="absolute inset-0 bg-black/50"></div>
+            <div className='relative min-h-screen overflow-hidden'>
+           <video src={cinema} autoPlay muted loop  playsInline className="absolute inset-0  w-full h-full object-cover " />
+              <div className="absolute inset-0  bg-black/50"></div>
                      <div className='relative z-10 md:max-w-7xl lg:max-w-7xl text-white pt-70 space-y-8 px-5 md:px-15 lg:px-25'>
                      <Link to={'/itsolution'}>  <h1 className='text-5xl md:text-7xl font-medium '>CINEMA & AV SOLUTIONS</h1></Link> 
                        <p className=' md:w-130 text-xl'>From consultation and design to installation and integration, we deliver tailored audiovisual solutions that combine premium equipment, seamless control, and an immersive experience for any space.</p>
--- src/pages/ElvHome.jsx
+++ src/pages/ElvHome.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import background from '../assets/images/ELVBACKGROUND.png'
-import Services from '../components/ElvhomeServices'
+import home from '../assets/videos/home.mp4'
 import Footer from '../components/Footer'
 import ElvhomeServices from '../components/ElvhomeServices'
 import { Link } from 'react-router-dom'
 
 export default function ElvHome() {
   return (
-    <div  >
-        <div style={{ backgroundImage: `url(${background})` }} className="relative bg-cover bg-center min-h-screen"  >
+    <div >
+      <div className='relative  min-h-screen overflow-hidden'>
+        <video src={home} autoPlay muted loop  playsInline className="absolute  inset-0 w-full h-full object-cover" />
          <div className="absolute inset-0 bg-black/50"></div>
-          <div className='relative z-10 md:max-w-7xl lg:max-w-7xl text-white pt-50 space-y-8 px-5 md:px-15 lg:px-25'>
+          <div className='relative z-10 md:max-w-7xl lg:max-w-7xl text-white pt-70 space-y-8 px-5 md:px-15 lg:px-25'>
            <Link to={'/cinema'}> <h1 className='text-5xl md:text-7xl font-medium '>HOME & BUILDING AUTOMATION SOLUTIONS</h1></Link>
           <p className=' md:w-130 text-xl'>Our expert team designs and integrates systems to deliver seamless, reliable, and effortless experiences across residential and commercial spaces.</p>
             </div>
+       
         </div>
         <ElvhomeServices/>
         <Footer/>
