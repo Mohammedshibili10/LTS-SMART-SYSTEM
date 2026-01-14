bd66d0feaf7d5d230bb108d079b3f8a7da29d2f4
Mohammedshibili10 <shibiliek516@gmail.com>
2026-01-14 21:56:08 +0530

    shadow updated

--- src/components/ElvhomeServices.jsx
+++ src/components/ElvhomeServices.jsx
@@ -13,13 +13,13 @@ export default function ElvhomeServices() {
 
 
                 return (
-                    <div key={index} className={`lg:flex items-center  gap-5 xl:gap-10  px-5 md:px-15 xl:px-24 py-10 xl:py-20  ${reverse ? "lg:flex-row-reverse" : ""}`} >
+                    <div key={index} className={`lg:flex   gap-5 lg:gap-10  px-5 md:px-18 xl:px-24 py-10 xl:py-20  ${reverse ? "lg:flex-row-reverse" : ""}`} >
                         <div className='flex-1  relative  px-5 sm:px-8'>
-                             <div className={`${isLeft ? 'absolute ml-4 md:ml-7  w-50 md:w-80 h-50 md:h-100 rounded-xl bg-gradient-to-b from-[#76B8289E]  -left-0 bg-gradient-to-b from-[#76B8289E]  bg-gradient-to-r from-[#76B8289E]  mask-b-from-20% mask-b-to-80%' : 'absolute   md:ml-5  w-50 md:w-80 h-50 md:h-80 rounded-xl bg-gradient-to-b from-[#76B8289E] -right-1 bg-gradient-to-b from-[#76B8289E] bg-gradient-to-l from-[#76B8289E] mask-b-from-20% mask-b-to-80% '}`}></div>
-                            <img className='relative left-3 md:left-5 top-3 md:top-5 sm:w-85  md:w-180 lg:w-190 lg:h-130 mr-1  ' src={item.image} alt="error" />
+                             <div className={`absolute${isLeft ? ' ml-4 md:ml-7  w-50 md:w-80 h-50 md:h-100 rounded-xl bg-gradient-to-b from-[#76B8289E]  -left-0 bg-gradient-to-b from-[#76B8289E]  bg-gradient-to-r from-[#76B8289E]  mask-b-from-20% mask-b-to-80%' : '       w-50 md:w-80 h-50 md:h-100  rounded-xl bg-gradient-to-b from-[#76B8289E] -right-1 bg-gradient-to-b from-[#76B8289E] bg-gradient-to-l from-[#76B8289E] mask-b-from-20% mask-b-to-80% '}`}></div>
+                            <img className='relative left-3 md:left-5 top-3 md:top-5 sm:w-85  md:w-180 lg:w-200 lg:h-130 mr-1  ' src={item.image} alt="error" />
                        </div>
 
-                        <div className='  space-y-8 mt-12  flex-1 px-5 md:px-15 lg:px-0 '>
+                        <div className='  space-y-8 mt-10   flex-1 md:px-10 lg:px-0  '>
                             <h1 className='text-3xl md:text-4xl lg:text-5xl font-semibold'>{item.title}</h1>
                             <p className='text-lg md:text-xl lg:w-130 '>{item.description}</p>
                             <ul className='space-y-5 max-w-xl'>
